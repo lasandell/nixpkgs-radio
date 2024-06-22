e73e5864e45b16f344ae90a6af4fcd85d459bd4d
Luke Sandell <lasandell@gmail.com>
2024-06-22 16:15:05 -0500

    dump1090-sdrplay: init at unstable-2023-08-28

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,4 @@
 final: prev: {
     qttermtcp =  prev.libsForQt5.callPackage ../pkgs/qttermtcp.nix {};
+    dump1090-sdrplay = prev.callPackage ../pkgs/dump1090-sdrplay.nix {};
 }
---
+++ pkgs/dump1090-sdrplay.nix
@@ -0,0 +1,38 @@
+{ lib, stdenv, fetchFromGitHub, pkg-config, librtlsdr, sdrplay }:
+
+stdenv.mkDerivation rec {
+  pname = "dump1090-sdrplay";
+  version = "unstable-2023-08-28";
+
+  src = fetchFromGitHub {
+    owner = "SDRplay";
+    repo = "dump1090";
+    rev = "b5a60d233057c5b904db3950efa29b6f1393fa2c";
+    sha256 = "sha256-dMtq1ZCUUDaW4Wp/hG5xndvLCR9B9zE/s2y/HB/Quzc=";
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ librtlsdr sdrplay ];
+
+  postPatch = ''
+    substituteInPlace Makefile --replace-fail 'PKG_CONFIG_PATH=' '#PKG_CONFIG_PATH='
+  '';
+
+  buildFlags = ["PREFIX=${placeholder "out"}"];
+
+  doCheck = true;
+
+  installPhase = ''
+    mkdir -p $out/bin $out/share
+    cp -v dump1090 view1090 $out/bin
+    cp -vr public_html $out/share/dump1090
+  '';
+
+  meta = with lib; {
+    description = "ADS-B Ground Station System Decoder";
+    homepage = "https://github.com/SDRplay/dump1090";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
