f7c1f16e1b28e6d3a8874366aee3e64a39348a95
Luke Sandell <lasandell@gmail.com>
2024-06-23 06:56:13 -0500

    libacars: init at 2.2.0

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,5 @@
 final: prev: {
+    libacars =  prev.callPackage ../pkgs/libacars.nix {};
     qttermtcp =  prev.libsForQt5.callPackage ../pkgs/qttermtcp.nix {};
     dump1090-sdrplay = prev.callPackage ../pkgs/dump1090-sdrplay.nix {};
 }
