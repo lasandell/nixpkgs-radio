cf518107bd66fb971d128cb0b1b4bfcb60d58321
Luke Sandell <lasandell@gmail.com>
2024-06-23 10:22:13 -0500

    dumphfdl: init at 1.6.1

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,6 @@
 final: prev: {
     acarsdec =  prev.callPackage ../pkgs/acarsdec.nix {};
+    dumphfdl = prev.callPackage ../pkgs/dumphfdl.nix {};
     libacars =  prev.callPackage ../pkgs/libacars.nix {};
     qttermtcp =  prev.libsForQt5.callPackage ../pkgs/qttermtcp.nix {};
     dump1090-sdrplay = prev.callPackage ../pkgs/dump1090-sdrplay.nix {};
