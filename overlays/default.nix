11cfa86072f920a1d69da38ce4655035e1b775cc
Luke Sandell <lasandell@gmail.com>
2024-08-06 11:45:19 -0500

    libax25-ve7fet: init at 1.2.2

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,7 @@ final: prev: {
     dump1090-sdrplay = prev.callPackage ../pkgs/dump1090-sdrplay.nix {};
     dumphfdl = prev.callPackage ../pkgs/dumphfdl.nix {};
     libacars =  prev.callPackage ../pkgs/libacars.nix {};
+    libax25-ve7fet =  prev.callPackage ../pkgs/libax25-ve7fet.nix {};
     qttermtcp =  prev.libsForQt5.callPackage ../pkgs/qttermtcp.nix {};
     sdrconnect = prev.callPackage ../pkgs/sdrconnect.nix {};
     sdrplay2 =  prev.callPackage ../pkgs/sdrplay2.nix {};
---
+++ pkgs/libax25-ve7fet.nix
@@ -0,0 +1,30 @@
+{ stdenv, lib, fetchFromGitHub, autoreconfHook, zlib }:
+
+stdenv.mkDerivation rec {
+  pname = "libax25-ve7fet";
+  version = "1.2.2";
+
+  src = fetchFromGitHub {
+    owner = "ve7fet";
+    repo = "linuxax25";
+    rev = "libax25-${version}";
+    sha256 = "sha256-5fGknA53JwmQk5/ak46J6v5aOwd/Na0oW7kfD6dGUdQ=";
+  };
+
+  sourceRoot = "source/libax25";
+
+  nativeBuildInputs = [ autoreconfHook ];
+
+  buildInputs = [ zlib ];
+
+  postPatch = ''
+    substituteInPlace pathnames.h --replace 'AX25_SYSCONFDIR"' '"/etc/ax25/'
+  '';
+
+  meta = with lib; {
+    description = "AX.25 library for ham radio applications (ve7fet fork)";
+    homepage = "https://github.com/ve7fet/linuxax25";
+    license = licenses.lgpl21Only;
+    platforms = platforms.linux;
+  };
+}
