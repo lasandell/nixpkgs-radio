f75f75794385ecdaed00b100a4a33678c8c1ef52
Luke Sandell <lasandell@gmail.com>
2024-08-08 21:06:50 -0500

    qttermtcp: 0.0.0.39 -> 0.0.0.73

--- pkgs/qttermtcp.nix
+++ pkgs/qttermtcp.nix
@@ -1,27 +1,49 @@
-{ lib, stdenv, fetchurl, qmake, qtbase, unzip, wrapQtAppsHook, }:
+{ lib, stdenv, fetchFromGitHub, copyDesktopItems, iconConvTools,
+  makeDesktopItem, qmake, qtmultimedia, qtserialport, qtbase, wrapQtAppsHook }:
 
+let
+  rev = "0.73";
+in
 stdenv.mkDerivation rec {
   pname = "qttermtcp";
-  version = "0.0.0.39";
+  version = "0.0.${rev}";
 
-  src = fetchurl {
-    url = "https://www.cantab.net/users/john.wiseman/Downloads/QtTermSource.zip";
-    hash = "sha256-PkR84kR9qRFFf/N/pEkq6TtiOihHGBFFQaQdVz60mdk=";
+  src = fetchFromGitHub {
+    owner = "g8bpq";
+    repo = "QtTermTCP";
+    hash = "sha256-omvJ4jOQnEDc2aPqT8LkJDpo8iWsLoREyqEmq959TA8=";
+    inherit rev;
   };
 
-  nativeBuildInputs = [ qmake unzip wrapQtAppsHook ];
+  nativeBuildInputs = [ copyDesktopItems iconConvTools qmake wrapQtAppsHook ];
 
-  buildInputs = [ qtbase ];
+  buildInputs = [ qtbase qtmultimedia qtserialport ];
 
-  setSourceRoot = "sourceRoot=`pwd`";
+  qmakeFlags = [
+    "QMAKE_CXXFLAGS+=-Wno-error=format-security"
+  ];
 
-  installPhase = ''
+  postInstall = ''
     mkdir -p $out/bin
     cp QtTermTCP $out/bin
+    icoFileToHiColorTheme QtTermTCP.ico QtTermTCP $out
   '';
 
+  desktopItems = with meta; [
+    (makeDesktopItem {
+      name = "QtTermTCP";
+      icon = "QtTermTCP";
+      exec = "QtTermTCP";
+      desktopName = "QtTermTCP";
+      genericName = "Packet Radio Client";
+      categories = [ "HamRadio" ];
+      keywords = [ "Ham" "Radio" "Packet"];
+      comment = description;
+    })
+  ];
+
   meta = with lib; {
-    description = "QtTermTCP is a multi-platform version of BPQTermTCP";
+    description = "AX.25 packet radio client based on BPQTermTCP";
     homepage = "https://www.cantab.net/users/john.wiseman/Documents/QtTermTCP.html";
     license = licenses.unfree;
     platforms = platforms.linux;
