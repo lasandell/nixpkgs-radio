15ecf33abe7807fa67026f1826e0b663c1bd85f4
Luke Sandell <lasandell@gmail.com>
2024-08-09 09:38:06 -0500

    qttermtcp: various fixes

--- pkgs/qttermtcp.nix
+++ pkgs/qttermtcp.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, copyDesktopItems, iconConvTools,
+{ lib, stdenv, fetchFromGitHub, copyDesktopItems, iconConvTools, makeWrapper,
   makeDesktopItem, qmake, qtmultimedia, qtserialport, qtbase, wrapQtAppsHook }:
 
 let
@@ -11,22 +11,33 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "g8bpq";
     repo = "QtTermTCP";
-    hash = "sha256-omvJ4jOQnEDc2aPqT8LkJDpo8iWsLoREyqEmq959TA8=";
-    inherit rev;
+    hash = "sha256-TF6qc7bN9dVlef+wRyF8Y1ox16bViZLNjgNRqeJCnds=";
+    # 0.73 tag points to 0.0.0.71 commit, so we use the hash instead
+    rev = "e1ed8e1fb27378046f3ab390ef07b87a26cb6ced"; # inherit rev;
   };
 
   nativeBuildInputs = [ copyDesktopItems iconConvTools qmake wrapQtAppsHook ];
 
   buildInputs = [ qtbase qtmultimedia qtserialport ];
 
-  qmakeFlags = [
-    "QMAKE_CXXFLAGS+=-Wno-error=format-security"
-  ];
+  postPatch = ''
+    substituteInPlace QtTermTCP.cpp \
+      --replace-fail 'qDebug(datas.data())' 'qDebug() << datas.data()'
+  '';
 
   postInstall = ''
     mkdir -p $out/bin
     cp QtTermTCP $out/bin
     icoFileToHiColorTheme QtTermTCP.ico QtTermTCP $out
+  '';
+
+  # Force QtTermTCP.ini to be placed in ~/.config
+  # Have to double wrap due to wrapQtAppsHook not supporting --run
+  postFixup = ''
+    source ${makeWrapper}/nix-support/setup-hook
+    wrapProgram $out/bin/QtTermTCP \
+      --run 'mkdir -p "$HOME/.config"' \
+      --run 'cd "$HOME/.config"'
   '';
 
   desktopItems = with meta; [
@@ -44,6 +55,10 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "AX.25 packet radio client based on BPQTermTCP";
+    longDescription = ''
+      Binary is wrapped to set the working directory to ~/.config/QTermTCP
+      so that it writes its config files there.
+    '';
     homepage = "https://www.cantab.net/users/john.wiseman/Documents/QtTermTCP.html";
     license = licenses.unfree;
     platforms = platforms.linux;
