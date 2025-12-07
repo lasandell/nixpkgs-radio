bef5a4d10a3cfdf1949bbade2beac6643a084df4
Luke Sandell <lasandell@gmail.com>
2025-12-07 07:20:15 -0600

    temp

--- flake.nix
+++ flake.nix
@@ -1,33 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
-    fup.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "github:edolstra/flake-compat";
   };
-  outputs = { self, fup, nixpkgs, ... }@inputs:
-    fup.lib.mkFlake {
-      inherit self inputs;
 
-      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
-
-      channelsConfig.allowUnfree = true;
-
-      sharedOverlays = [ self.overlays.default ];
-
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      overlay = import ./overlays;
+    in
+    {
       overlays.default = import ./lib/pinOverlay.nix {
-        inherit nixpkgs;
-        overlay = import ./overlays;
+        inherit nixpkgs overlay;
       };
 
       nixosModules = {
         sdrplay = import ./nixosModules/sdrplay.nix;
         sdrplay2 = import ./nixosModules/sdrplay2.nix;
       };
+    }
+    //
+    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ overlay ];
+        };
+        overlayPkgs = overlay pkgs pkgs;
+      in
+      {
+        packages = nixpkgs.lib.filterAttrs (n: _: builtins.hasAttr n overlayPkgs) pkgs;
 
-      outputsBuilder = channels: {
-        packages = fup.lib.exportPackages { inherit (self.overlays) default; } channels;
-
-        devShells.default = import ./devShells (channels.nixpkgs);
-      };
-    };
+        devShells.default = import ./devShells pkgs;
+      }
+    );
 }
